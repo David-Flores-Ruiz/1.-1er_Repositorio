da597d2ff53d8eca1825e2bad6777e1ea1a30615
David-Flores-Ruiz <39530033+David-Flores-Ruiz@users.noreply.github.com>
2018-05-22 12:35:37 -0500

    Cifrado César

---
+++ 3.- Sesion_2 - Cifrado Cesar/src/3.- Sesion_2 - Cifrado Cesar.c
@@ -0,0 +1,35 @@
+/*
+ ============================================================================
+ Name        : -.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	char cadenaCaracteres[10];
+
+	scanf("%s", cadenaCaracteres);
+
+	if(cadenaCaracteres >= 65 & cadenaCaracteres <= 90)	//Validar Mayúsculas
+		if (cadenaCaracteres >= 97 & cadenaCaracteres <= 122)	//Validar Minúsculas
+	for(int i=0; i<10; i++)
+	{
+
+
+	}
+
+
+
+		printf("La cadena original es: %s", cadenaCaracteres);
+
+
+
+	return EXIT_SUCCESS;
+}
